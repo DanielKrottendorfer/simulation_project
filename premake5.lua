44b63868f1ff898794e930a22ce91bbcf7f14472
DanielKrottendorfer <krotttendorfer@hotmail.de>
2022-12-20 11:22:43 +0100

    add imgui

--- premake5.lua
+++ premake5.lua
@@ -1,5 +1,10 @@
-libdirs { "./extern/SDL2/lib/x64"}
-includedirs { "./extern/SDL2/include", "./extern/glad/include", "./extern/glm","./extern/lodepng"}
+libdirs {"./extern/SDL2/lib/x64"}
+includedirs {
+   "./extern/SDL2/include",
+   "./extern/glad/include",
+   "./extern/glm",
+   "./extern/lodepng",
+   "./extern/imgui"}
 
 workspace "HelloWorld"
    configurations { "Debug", "Release" }
@@ -11,7 +16,14 @@ project "HelloWorld"
    targetdir "bin/%{cfg.buildcfg}"
    links {"SDL2main", "SDL2"}
 
-   files { "**.h", "**.c", "**.cpp", "**.hpp" }
+   files { "./src/**.h", 
+      "./src/**.c", 
+      "./src/**.cpp", 
+      "./src/**.hpp" }
+
+   files {"./extern/glad/src/*.c"}
+   files {"./extern/lodepng/*.cpp"}
+   files {"./extern/imgui/*.cpp"}
 
    filter "configurations:Debug"
       defines { "DEBUG" }
